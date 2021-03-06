2fa4de180701fb311e39a977eedb907658416313
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-03-06 19:46:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ op: Is both the x and y is equal or not true
 Or use comma:
 console.log("is both the x and y is equal or not ", x==y)
 op: is both the x and y is equal or not true
+---------------------------------------------------------------------------------------------------
+  
+ // operator
+// Increment (postfix)
+var num =15;
+newNum= num++ + 5;
+console.log(num);
+console.log(newNum);  
+//In postfix the value first assign then increment
+
+// Decerment operator (prifix)
+
+var num =18;
+newNum= ++num +4;
+console.log(num);
+console.log(newNum)
+
+// in prefix the value first increment then print.
