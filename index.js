cfff8eb85b046ef538c1644f6a79d2d66f93ea82
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-03-12 23:11:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -120,3 +120,8 @@ function myCalculator(num1, num2, myCallback) {
 }
 
 myCalculator(5, 5, myDisplayer);
+// JavaScript Array
+
+var cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+document.getElementById("demo").innerHTML = cars[0];
