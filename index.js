77dafd1cbdb10f03272c1fd4ee5997bb80396799
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-03-25 23:59:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,3 +166,28 @@ function display_array()
 // Note: map() does not execute the function for array elements without values.
 
 // Note: this method does not change the original array.
+-----------------------------------------------------------------------------------------
+  
+  Example:-
+    
+  <!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to get the square root of each element in the array.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+var numbers = [4, 9, 16, 25];
+
+function myFunction() {
+  x = document.getElementById("demo")
+  x.innerHTML = numbers.map(Math.sqrt);
+}
+</script>
+
+</body>
+</html>
