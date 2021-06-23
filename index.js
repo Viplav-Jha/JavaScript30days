847806cb3b522064c61fa7498e93a53d6fa01cfc
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-06-23 23:37:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -220,3 +220,4 @@ else {
 // generating a random number
 const a = Math.random() * (10-1) + 1
 console.log(`Random value between 1 and 10 is ${a}`);
+-------------------------------------------------------------------------------------
