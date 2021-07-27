f89ce19bc2d804680b323e338cbfee2325221154
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-07-27 23:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var y= 5;
 console.log("Is both x and y equal or not " + x==y);
 output: false
 console.log("Is both x and y equal or not, (x==y));
-output: true
+output: true ;
 --------------------------------------------------
 Reason behind You are concatenating "Is both the x and y is equal or not" with x first and comparing with y. This will give you false.
 You can put a bracket around x==y. 
